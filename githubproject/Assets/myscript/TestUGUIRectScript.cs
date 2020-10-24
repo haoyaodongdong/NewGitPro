586dd4fb7bbd8a47501d9660d4c5e983ae3e42ee
haoyadong <1750238275@qq.com>
2020-10-24 17:23:39 +0800

    提交所有的信息

---
+++ githubproject/Assets/myscript/TestUGUIRectScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestUGUIRectScript : MonoBehaviour
+{
+    public RectTransform trans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ButtonEvent()
+    {
+        Debug.Log("rect.anchorpostion" + trans.anchoredPosition);
+        Debug.Log("rec.offmax" + trans.anchorMax);
+        Debug.Log("rect.offmin" + trans.offsetMin);
+    }
+}
